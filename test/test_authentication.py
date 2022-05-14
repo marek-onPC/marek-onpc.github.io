bf9baefb56ae2c6073e448fe7cd410b01cd365bb
Marek Smieja <67819687+marek-onPC@users.noreply.github.com>
2022-05-14 14:13:27 +0900

    Add backend test for authentication

---
+++ test/test_authentication.py
@@ -0,0 +1,31 @@
+import pytest
+from auth.authentication import Authentication
+
+authentication = Authentication()
+
+@pytest.mark.parametrize(
+    "password", [
+        "uhhs6hsbja",
+        "AHg%&JNhsjkdnh&89sh",
+        "7262hskdd"
+    ]
+)
+def test_hash_and_verify_password(password: str) -> None:
+    hashed_password = authentication.hash_password(password)
+
+    assert authentication.verify_password(password, hashed_password) == True
+
+
+@pytest.mark.parametrize(
+    "username", [
+        "new0user",
+        "user@name.com",
+        "test56@user.com"
+    ]
+)
+def test_encode_and_decode_jwt(username: str):
+    jwt = authentication.encode_jwt(username)
+    decoded_jwt = authentication.decode_jwt(jwt)
+
+    assert decoded_jwt == username
+
