5194304d516c32397c677ede5c51cdeadb9983e8
Marek Smieja <67819687+marek-onPC@users.noreply.github.com>
2022-05-15 12:26:44 +0900

    Add login (with db) API route; Cleanup routes

---
+++ api/login.py
@@ -0,0 +1,36 @@
+import os
+from typing import Dict
+from dotenv import load_dotenv
+from fastapi import APIRouter, HTTPException
+from auth.authentication import Authentication
+from auth.schemas import AuthDetails
+from db.db_client import DatabaseClient
+
+load_dotenv()
+
+db_uri = os.environ["DB_URI"]
+db_name = os.environ["DB_NAME"]
+db_client = DatabaseClient(db_uri, db_name)
+
+router = APIRouter()
+authentication = Authentication()
+
+
+@router.post("/login")
+def login(auth_details: AuthDetails) -> Dict:
+    collection_name = "users"
+    collection = db_client.db_connection(collection_name)
+
+    user = collection.find_one({
+        "user": auth_details.username
+    })
+
+    if user is None:
+        raise HTTPException(status_code=401, detail="Wrong user")
+
+    if not authentication.verify_password(auth_details.password, user["password"]):
+        raise HTTPException(status_code=401, detail="Wrong password")
+    
+    token = authentication.encode_jwt(user["user"])
+
+    return { "token" : token }
--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import os
 from dotenv import load_dotenv
-from typing import Dict
-from fastapi import Depends, FastAPI, HTTPException
+from fastapi import FastAPI
 from auth.authentication import Authentication
-from auth.schemas import AuthDetails
-from db.db_client import DatabaseClient
-import bson.json_util as json_util
+
+from api import login
 
 load_dotenv()
 
@@ -14,40 +12,5 @@ db_name = os.environ["DB_NAME"]
 
 app = FastAPI()
 authentication = Authentication()
-db_client = DatabaseClient(db_uri, db_name)
 
-user = {
-    "username": "name",
-    "hashedPassword": "$2b$12$jwzb6rl2uspKNkJqNhv.z.kC36N.F3tQ5JzfywEgp422hufoHc7/C"
-    # password is "password"
-}
-
-@app.post("/login")
-def login(auth_details: AuthDetails):
-    if auth_details.username != user["username"]:
-        raise HTTPException(status_code=401, detail="Wrong user")
-
-    if not authentication.verify_password(auth_details.password, user["hashedPassword"]):
-        raise HTTPException(status_code=401, detail="Wrong password")
-    
-    token = authentication.encode_jwt(user["username"])
-
-    return { "token" : token }
-
-
-@app.get("/noauth")
-def no_auth() -> str:
-    return "No need to auth"
-
-
-@app.get("/withauth")
-def with_auth(username = Depends(authentication.auth_wrapper)) -> Dict:
-    return { "name" : username }
-
-
-@app.get("/login")
-def no_auth() -> Dict:
-    response = db_client.db_find_one("users", {"user" : "mareksmieja@gmail.com"})
-    response_json = json_util._json_convert(response)
-
-    return response_json
+app.include_router(login.router)
