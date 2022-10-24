d0fff4479d62a831364b8ac443075499b9f26f73
Marek Smieja <mareksmieja@gmail.com>
2022-10-24 21:42:32 +0900

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ db_uri = os.environ["DB_URI"]
 db_name = os.environ["DB_NAME"]
 
 origins = [
-    "https://marek-onpc.github.io/site-and-notes",
+    "https://marek-onpc.github.io",
 ]
 
 app = FastAPI()
