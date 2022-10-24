2fdf135ee051a664d0463f69b66ec4026f9a4d7a
Marek Smieja <mareksmieja@gmail.com>
2022-10-24 21:35:17 +0900

    Add CORS

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from auth.authentication import Authentication
 
 from api import login, authpath, notes, note
@@ -10,9 +11,20 @@ load_dotenv()
 db_uri = os.environ["DB_URI"]
 db_name = os.environ["DB_NAME"]
 
+origins = [
+    "https://site-and-notes.herokuapp.com",
+]
+
 app = FastAPI()
 authentication = Authentication()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(login.router)
 app.include_router(authpath.router)
 app.include_router(notes.router)
