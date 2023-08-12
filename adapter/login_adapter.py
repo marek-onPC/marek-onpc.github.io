e7f8c596f19f17fe343fe2ec84402be7e80a63a5
Marek Smieja <mareksmieja@gmail.com>
2023-08-13 08:59:58 +0900

    Add Docker (python server)

--- adapter/login_adapter.py
+++ adapter/login_adapter.py
@@ -1,8 +1,8 @@
 import os
+from typing import Optional
 from dotenv import load_dotenv
 from db.db_client import DatabaseClient
 import schemas.schemas
-from fastapi import UploadFile
 
 load_dotenv()
 
@@ -12,7 +12,7 @@ db_collection_name = os.environ["DB_COLL_USERS"]
 db_client = DatabaseClient(db_uri, db_name)
 
 
-def login(username: str) -> schemas.schemas.User | None:
+def login(username: str) -> Optional[schemas.schemas.User]:
     collection = db_client.db_connection(db_collection_name)
 
     user = db_client.db_find_one(collection, {
