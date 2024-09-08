64dc79f875551b415bf036edf7b8cea8ffd50752
Marek Smieja <67819687+marek-onPC@users.noreply.github.com>
2024-09-08 18:33:34 +0900

    Fetch client

---
+++ new-client/src/lib/fetchClient.ts
@@ -0,0 +1,57 @@
+import { HTTPMethods } from "../types";
+import { PUBLIC_APP_SERVER } from "$env/static/public"
+
+const fetchClientPostWithoutToken = async (
+  url: string,
+  data: object
+): Promise<any> => {
+  const response = await fetch(`${PUBLIC_APP_SERVER}${url}`, {
+    method: HTTPMethods.POST,
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+  })
+    .then(async (data) => {
+      if (data.status !== 200) {
+        throw new Error(data.statusText);
+      }
+      const responseStatus = data.status;
+      const responseData = await data.json();
+
+      return {
+        status: responseStatus,
+        data: responseData
+      };
+    })
+
+  return response;
+}
+
+const fetchClientGetWithoutToken = async (
+  url: string,
+): Promise<any> => {
+  const response = await fetch(`${PUBLIC_APP_SERVER}${url}`, {
+    method: HTTPMethods.GET,
+    headers: {
+      "Content-Type": "application/json"
+    },
+  })
+    .then(async (data) => {
+      if (data.status !== 200) {
+        throw new Error(data.statusText);
+      }
+      const responseStatus = data.status;
+      const responseData = await data.json();
+
+      return {
+        status: responseStatus,
+        data: responseData
+      };
+    })
+
+  return response;
+}
+
+
+export { fetchClientPostWithoutToken, fetchClientGetWithoutToken }
