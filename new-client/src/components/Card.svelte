156eb89fc484bfd3271be24d69773f79fdd36356
Marek Smieja <67819687+marek-onPC@users.noreply.github.com>
2024-11-30 16:01:27 +0900

    Add Card component, adjust Dashboard

---
+++ new-client/src/components/Card.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import type { CheatSheetCardType } from '../types';
+
+  export let cheatSheetData: CheatSheetCardType;
+</script>
+
+<div class="card">
+  <h4>{cheatSheetData.title}</h4>
+  <p>{cheatSheetData.is_published ? 'Published' : 'Draft'}</p>
+  <div>
+    {#if cheatSheetData.categories}
+      <p>Categories:</p>
+      {#each cheatSheetData.categories as category}
+        <p>{category}</p>
+      {/each}
+    {/if}
+  </div>
+</div>
--- new-client/src/routes/dashboard/+page.svelte
+++ new-client/src/routes/dashboard/+page.svelte
@@ -1,1 +1,32 @@
-<p>Placeholder</p>
+<script lang="ts">
+  import { fetchClientGet } from '$lib/fetchClient';
+  import { onMount } from 'svelte';
+  import type { CheatSheetWithContentType } from '../../types';
+  import { sessionToken } from '../../stores';
+  import Card from '../../components/Card.svelte';
+
+  let cheatSheetsData: Array<CheatSheetWithContentType>;
+
+  const loadCheatSheets = async () => {
+    try {
+      const raw = await fetchClientGet('/cheat_sheets', $sessionToken);
+      cheatSheetsData = JSON.parse(raw.data) as Array<CheatSheetWithContentType>;
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  onMount(() => {
+    loadCheatSheets();
+  });
+</script>
+
+<div>
+  {#if cheatSheetsData}
+    {#each cheatSheetsData as card}
+      <Card cheatSheetData={card} />
+    {/each}
+  {:else}
+    <p>Loading...</p>
+  {/if}
+</div>
