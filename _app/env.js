534943e6116554d43644d9c867b4dc5a7bd9d272
Marek Smieja <67819687+marek-onPC@users.noreply.github.com>
2025-01-04 13:25:09 +0900

    Updates

---
+++ _app/env.js
@@ -0,0 +1,1 @@
+export const env={"PUBLIC_APP_LOCAL_STORAGE_TOKEN":"san-3jshd7-token","PUBLIC_APP_LOCAL_STORAGE_EXPIRY":"san-m85naG-expiry","PUBLIC_APP_SERVER":"https://site-and-notes.herokuapp.com"}
---
+++ _app/immutable/entry/start.BrjIR3GX.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.KhD8MUh5.js";export{t as start};
