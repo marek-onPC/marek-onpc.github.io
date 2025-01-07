4f50a393cd07a8fc029808659508debe973070ff
Marek Smieja <67819687+marek-onPC@users.noreply.github.com>
2025-01-07 11:25:16 +0900

    [server] Refactor DB connection

--- server/domain/login_domain.py
+++ server/domain/login_domain.py
@@ -1,14 +1,48 @@
+import logging
+import os
+from typing import Any, Optional
+
+from dotenv import load_dotenv
 from fastapi import HTTPException
 
-from adapter import user_adapter
 from helpers.authentication import Authentication
-from schemas import AuthDetails, LoginToken
+from helpers.db_client import DatabaseClient
+from schemas import AuthDetails, LoginToken, User, Username
+
+load_dotenv()
+
+db_uri = os.environ["DB_URI"]
+db_name = os.environ["DB_NAME"]
+db_collection_name = os.environ["DB_COLL_USERS"]
+db_client = DatabaseClient(db_uri, db_name, db_collection_name).connection()
 
 authentication = Authentication()
 
 
+def _bson_user_to_object(bson_document: Any) -> User:
+    try:
+        username = bson_document["user"]
+    except Exception as e:
+        logging.error(e)
+        raise HTTPException(status_code=400, detail=e)
+
+    try:
+        password = bson_document["password"]
+    except Exception as e:
+        logging.error(e)
+        raise HTTPException(status_code=400, detail=e)
+
+    return User(username=username, password=password)
+
+
+def get_user(username: Username) -> Optional[User]:
+    user = db_client.find_one({"user": username})
+
+    return _bson_user_to_object(bson_document=user) if user else None
+
+
 def login(auth_details: AuthDetails) -> LoginToken:
-    user = user_adapter.get_user(username=auth_details.username)
+    user = get_user(username=auth_details.username)
 
     if user is None:
         raise HTTPException(status_code=401, detail="Wrong user")
