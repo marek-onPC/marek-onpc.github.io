00c1431a21cf0f882f45303256f1e7a4e41865ee
Marek Smieja <mareksmieja@gmail.com>
2025-05-06 09:21:54 +0900

    [client] Add long loading notification

--- client/src/routes/about/+page.svelte
+++ client/src/routes/about/+page.svelte
@@ -61,7 +61,7 @@
     margin: 10px;
 
     @media (min-width: 600px) {
-      margin: 50px;
+      margin: 50px auto;
     }
 
     p {
--- client/src/routes/sheets/+page.svelte
+++ client/src/routes/sheets/+page.svelte
@@ -4,20 +4,34 @@
   import type { CheatSheetWithContentType } from '../../types';
   import Card from '../../components/Card.svelte';
   import Loader from '../../components/Loader.svelte';
+  import { slide } from 'svelte/transition';
 
   let cheatSheetsData: Array<CheatSheetWithContentType>;
+  let isLongLoading = false;
 
   const loadCheatSheets = async () => {
     try {
       const raw = await fetchClientGetWithoutToken('/cheat_sheets');
       cheatSheetsData = raw.data as Array<CheatSheetWithContentType>;
+      isLongLoading = false;
     } catch (e) {
       console.log(e);
     }
   };
 
+  const checkLoading = () => {
+    if (cheatSheetsData) {
+      isLongLoading = false;
+    } else {
+      isLongLoading = true;
+    }
+  };
+
   onMount(() => {
     loadCheatSheets();
+    setTimeout(() => {
+      checkLoading();
+    }, 3000);
   });
 </script>
 
@@ -25,9 +39,16 @@
   There’s no strict rule for what ends up here — it’s just a <span class="highlight"
     >mix of code snippets</span
   > I’ve found handy over time. Some are super simple, others a bit more complex. A few came from real-world
-  projects, while others are just results from studying or experimenting. I put them here for easy access,
-  to quickly access them when needed.
+  projects, while others are just results from studying or experimenting. I put them here for easy access
+  when needed.
 </p>
+
+{#if isLongLoading}
+  <div class="sheets__notif" transition:slide={{ duration: 250 }}>
+    <p>⚡ Waking up the server... this might take a few seconds...</p>
+  </div>
+{/if}
+
 <div class="sheets__list">
   {#if cheatSheetsData}
     {#each cheatSheetsData as card}
@@ -51,6 +72,28 @@
       .highlight {
         font-size: 24px;
         color: #42b883;
+      }
+    }
+
+    &__notif {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: calc(100% - 40px);
+      max-width: 500px;
+      margin: 0 auto;
+      padding: 20px;
+      background-color: #42b883;
+      color: #fff;
+      font-size: 18px;
+      font-weight: 400;
+      border-radius: 5px;
+      position: absolute;
+      bottom: 25px;
+      left: 10px;
+
+      p {
+        margin: 0;
       }
     }
 
