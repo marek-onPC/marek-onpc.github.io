70ece1df29325cda7a6b85b26f07a9e7801f43f4
Marek Smieja <mareksmieja@gmail.com>
2025-06-21 08:10:59 +0900

    Updates

--- _app/env.js
+++ _app/env.js
@@ -1,1 +1,1 @@
-export const env={"PUBLIC_APP_LOCAL_STORAGE_TOKEN":"san-3jshd7-token","PUBLIC_APP_LOCAL_STORAGE_EXPIRY":"san-m85naG-expiry","PUBLIC_APP_SERVER":"https://site-and-notes.herokuapp.com"}
+export const env={"PUBLIC_APP_LOCAL_STORAGE_TOKEN":"san-3jshd7-token","PUBLIC_APP_LOCAL_STORAGE_EXPIRY":"san-m85naG-expiry","PUBLIC_APP_LOCAL_STORAGE_REFRESH":"san-6sU5gl-refresh","PUBLIC_APP_SERVER":"http://localhost:8000"}
---
+++ _app/immutable/entry/start.DaVQKrVf.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.3MMd-dIw.js";export{t as start};
