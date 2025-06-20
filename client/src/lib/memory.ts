3c3ce6b30715e25bfe700f05a78b839de5bd1ce3
Marek Smieja <mareksmieja@gmail.com>
2025-06-20 22:45:15 +0900

    [server] Adjusted memory storage keys

--- client/src/lib/memory.ts
+++ client/src/lib/memory.ts
@@ -1,16 +1,19 @@
 import {
   PUBLIC_APP_LOCAL_STORAGE_TOKEN,
-  PUBLIC_APP_LOCAL_STORAGE_EXPIRY
+  PUBLIC_APP_LOCAL_STORAGE_EXPIRY,
+  PUBLIC_APP_LOCAL_STORAGE_REFRESH
 } from '$env/static/public';
 import type { AuthToken } from '../types';
 
-const setTokenInMemory = ({ token, expiry }: AuthToken): void => {
-  window.localStorage.setItem(PUBLIC_APP_LOCAL_STORAGE_TOKEN, token);
-  window.localStorage.setItem(PUBLIC_APP_LOCAL_STORAGE_EXPIRY, expiry.toISOString());
+const setTokenInMemory = ({ accessToken, refreshToken, expiry }: AuthToken): void => {
+  window.localStorage.setItem(PUBLIC_APP_LOCAL_STORAGE_TOKEN, accessToken);
+  window.localStorage.setItem(PUBLIC_APP_LOCAL_STORAGE_REFRESH, refreshToken);
+  window.localStorage.setItem(PUBLIC_APP_LOCAL_STORAGE_EXPIRY, expiry ? expiry.toISOString() : '');
 };
 
 const getTokenFromMemory = (): AuthToken => {
-  const token = window.localStorage.getItem(PUBLIC_APP_LOCAL_STORAGE_TOKEN) || '';
+  const accessToken = window.localStorage.getItem(PUBLIC_APP_LOCAL_STORAGE_TOKEN) || '';
+  const refreshToken = window.localStorage.getItem(PUBLIC_APP_LOCAL_STORAGE_REFRESH) || '';
   const expiry = window.localStorage.getItem(PUBLIC_APP_LOCAL_STORAGE_EXPIRY) || '';
 
   let expiryDate = new Date();
@@ -18,14 +21,18 @@ const getTokenFromMemory = (): AuthToken => {
   if (expiry) {
     expiryDate = new Date(expiry);
   }
+
   return {
-    token: token,
-    expiry: expiryDate
+    accessToken: accessToken,
+    refreshToken: refreshToken,
+    expiry: expiryDate,
+    tokenType: 'Bearer'
   };
 };
 
 const removeTokenInMemory = (): void => {
   window.localStorage.removeItem(PUBLIC_APP_LOCAL_STORAGE_TOKEN);
+  window.localStorage.removeItem(PUBLIC_APP_LOCAL_STORAGE_REFRESH);
   window.localStorage.removeItem(PUBLIC_APP_LOCAL_STORAGE_EXPIRY);
 };
 
