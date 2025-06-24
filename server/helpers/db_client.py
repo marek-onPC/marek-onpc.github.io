2d3e75facf7f03f2231db3d2eb112b1370f2ab41
Marek Smieja <mareksmieja@gmail.com>
2025-06-25 08:36:32 +0900

    [server] Defined DB client as a singleton

--- server/helpers/db_client.py
+++ server/helpers/db_client.py
@@ -1,9 +1,22 @@
+from typing import Self
+
 from pymongo import MongoClient
 from pymongo.collection import Collection
 
 
 class DatabaseClient:
-    def __init__(self, uri: str, name: str, collection: str) -> None:
+    _INSTANCE: Self | None = None
+
+    def __new__(cls: type[Self], uri: str, name: str, collection: str) -> Self:
+        if cls._INSTANCE is None:
+            cls._INSTANCE = super(DatabaseClient, cls).__new__(cls)
+        return cls._INSTANCE
+
+    def __init__(self: Self, uri: str, name: str, collection: str) -> None:
+        if getattr(self, "_initialized", False):
+            return
+
+        self._initialized = True
         self.uri = uri
         self.name = name
         self.collection = collection
