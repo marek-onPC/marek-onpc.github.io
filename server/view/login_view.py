52c17bc8c62bef7c125d73bc28f69644b9fe91b1
Marek Smieja <mareksmieja@gmail.com>
2025-11-02 11:05:19 +0900

    [server] Added async logging on login

--- server/view/login_view.py
+++ server/view/login_view.py
@@ -2,8 +2,8 @@ from fastapi import APIRouter, Response
 
 from domain import login_domain
 from helpers.authentication import Authentication
-from schemas import AuthenticationDetails, AuthToken
-from helpers.events import log_event
+from helpers.events import EventTypes, send_log_event
+from schemas import AllowedGrandTypes, AuthenticationDetails, AuthToken
 
 router = APIRouter(prefix="/api")
 authentication = Authentication()
@@ -11,15 +11,58 @@ authentication = Authentication()
 
 @router.post("/login", response_model=AuthToken)
 def login(auth_details: AuthenticationDetails) -> Response:
-    log_event(event_type="login_attempt", description=f"Login attempt for user {auth_details.username}")
+    log_message = (
+        "Login attempt via password"
+        if auth_details.grant_type == AllowedGrandTypes.PASSWORD
+        else "Login attempt via refresh_token"
+    )
 
-    token = login_domain.get_token(auth_details=auth_details).model_dump_json()
+    send_log_event(
+        event_type=EventTypes.LOGIN_ATTEMPT,
+        message=log_message,
+        user=(
+            auth_details.username
+            if auth_details.grant_type == AllowedGrandTypes.PASSWORD
+            else None
+        ),
+        context={"status": "started", "grant_type": auth_details.grant_type},
+    )
+
+    try:
+        token = login_domain.get_token(auth_details=auth_details).model_dump_json()
+    except Exception as e:
+        send_log_event(
+            event_type=EventTypes.LOGIN_ATTEMPT,
+            message=log_message,
+            user=(
+                auth_details.username
+                if auth_details.grant_type == AllowedGrandTypes.PASSWORD
+                else None
+            ),
+            context={
+                "status": "failed",
+                "grant_type": auth_details.grant_type,
+                "error": str(e),
+            },
+        )
+        raise e
 
     response = Response(
         content=token,
         media_type="application/json",
         status_code=200,
         headers={"cache-control": "no-store"},
+    )
+
+    send_log_event(
+        event_type=EventTypes.LOGIN_ATTEMPT,
+        message=log_message,
+        user=(
+            auth_details.username
+            if auth_details.grant_type == AllowedGrandTypes.PASSWORD
+            else None
+        ),
+        context={"status": "successful", "grant_type": auth_details.grant_type},
     )
 
     return response
